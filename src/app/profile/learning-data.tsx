458270061f21b2e3400dc5cd0f4820095f421592
Nickname-kun <bunkeimathlove@gmail.com>
2025-05-29 11:25:28 +0900

    fix: access name property from contests array

--- src/app/profile/learning-data.tsx
+++ src/app/profile/learning-data.tsx
@@ -196,7 +196,7 @@ export default function LearningData({
                 <Td>
                   {vc.contest_id ? (
                     <ChakraLink as={Link} href={`/contests/${vc.contest_id}/virtual/${vc.id}`}>
-                      {vc.contests?.name || 'N/A'}
+                      {vc.contests?.[0]?.name || 'N/A'}
                     </ChakraLink>
                   ) : 'N/A'}
                 </Td>

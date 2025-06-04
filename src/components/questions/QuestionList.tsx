29eaeab868b72f7426ad9523a3042e413156a38e
Nickname-kun <bunkeimathlove@gmail.com>
2025-06-04 14:51:09 +0900

    debug: Add client-side logging to QuestionList

--- src/components/questions/QuestionList.tsx
+++ src/components/questions/QuestionList.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 import { Box, VStack } from '@chakra-ui/react';
 import { QuestionWithDetails } from '@/types/question';
 import QuestionCard from './QuestionCard';
+import { useEffect } from 'react';
 
 type QuestionListProps = {
   questions: QuestionWithDetails[];
 };
 
 export default function QuestionList({ questions }: QuestionListProps) {
+  useEffect(() => {
+    console.log('QuestionList received questions:', questions);
+  }, [questions]);
+
   return (
     <VStack spacing={4} align="stretch">
       {questions.map((question) => (
